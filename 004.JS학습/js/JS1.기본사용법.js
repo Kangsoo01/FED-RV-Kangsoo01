36c4318771142c71d44d24a99a3ff1a80e207397
Kangsoo01 <angelakang54@gmail.com>
2024-11-06 17:08:16 +0900

    24-11-06 업데이트

--- 004.JS학습/js/JS1.기본사용법.js
+++ 004.JS학습/js/JS1.기본사용법.js
@@ -1,13 +1,120 @@
-// JS1.기본사용법 JS //
+// JS1.기본사용법 JS ////////////
 
-console.log("JS1.기본사용법 시작");
+console.log("JS1.기본사용법 시작~!");
 
-/*******************************************
-    함수명: 김비서 나와라
-    기능: 김비서 박스에 각종 변경하기
-*******************************************/
+// 함수는 코드를 저장하는 구역임
+// 함수명은 코드저장소의 주소다!
+// 함수의 호출은 함수명()
+/**************************************** 
+    함수명 : 김비서나와라
+    기능 : 김비서박스에 각종 변경하기
+****************************************/
+function 김비서나와라() {
+    // 1. 함수호출확인!
+    console.log("김비서 나왔다~!");
+  
+    // 2. HTML넣기 : 이벤트 속성에서 인라인코드로
+    // 이미 실행함!(이미지 넣기)
+  
+    // 3. CSS변경하기 //////
+    // 대상선정 : .pbox
+    // 클래스는 여러개 사용할 수 있으므로
+    // HTML컬랙션 집합에 수집되어 사용됨!
+    // getElementsByClassName(클래스명)
+    // 0,1,2,... 이런 순서로 수집함
+    // 하나뿐이어도 0번째라고 해야함!
+    console.log(
+        document.getElementsByClassName(
+            "pbox"
+        )
+    );
+    
+    // 변수에 대상 할당하기
+    var 나변수 = 
+    document.getElementsByClassName('pbox').item(0);
+    // 변수는 특정 메모리 공간이다
+    // 선언과 동시에 할당하여 값을 저장한다
+    // 변수는 여기는 var("봐")로 선언
+    // var 변수명 = 값
+    
+    // 2. HTML넣기 : 이벤트 속성에서 인라인코드로
+    // 이미 실행함!(이미지 넣기)
+    
+    // 3. CSS변경하기 //////
+    // 대상선정 : .pbox
+    // 클래스는 여러개 사용할 수 있으므로
+    // HTML컬랙션 집합에 수집되어 사용됨!
+    // getElementsByClassName(클래스명)
+    // 0,1,2,... 이런 순서로 수집함
+    // 하나뿐이어도 0번째라고 해야함!
+    
+    //변수를 사용하는 이유는?
+    // - 변수는 재사용
+    // - 변수는 저장 메모리공간 주소
+    // - 변수는 따음표를 안쓴 문자
+    // - 변수는 띄어쓰기가 없다
+    // - 변수는 호출 시 값이 출력된다
+    // - 변수는 재할당시 다른 값이 출력된다
+    // - 변수는 한번만 선언 var(let/const)
+    // - var는 variable(변수)라는 단어에서 파생된 말이다
+    
+    //////////변경내용///////////
+    
+    // 3-1. 배경이미지 넣기
+    나변수.style.background = "url(./images/kimbs.jpg) no-repeat top/cover";
+    // .pbox중 첫번째 (0번)를 선택함!
+    // item(순번)
 
-function 김비서나와라(){
-    // 1. 함수 호출확인
-    console.log('김비서 나왔다');
-}//김비서나와라 함수
+    // 3-2. 글자색 변경하기
+    나변수.style.color ='lime';
+
+    // 3-3. 글자크기 변경하기
+    나변수.style.fontSize = '40px';
+
+    // 3-4. 글자 그림자
+    나변수.style.textShadow = "2px 2px 2px #000";
+
+    // 3-5. 트랜지션
+    나변수.style.transition = "all 1s";
+    // 백틱은 줄 바꿈이 자유롭다
+    나변수.style.transition = `
+    2s,
+    top 4s 2s,
+    left 2s 2s,
+    rotate 3s 3s,
+    translate 1s 2s,
+    scale 2s 6s
+    `;
+
+    // 3-6. 크기변경
+    나변수.style.width = "600px";
+    나변수.style.height = "600px";
+
+    // 3-7. 줄 간격
+    나변수.style.lineHeight = "100px";
+
+    // 3-8. 원 만들기
+    나변수.style.borderRadius = "50%";
+    
+    // 3-9. 중앙 이동을 위한 top, left 값 설정
+    나변수.style.top = "50%";
+    나변수.style.left = "50%";
+
+    // 3-10. 트랜스폼 변경: 중앙이동, 360도 회전, 스케일 변경
+    나변수.style.translate = "-50% -50%";
+    나변수.style.rotate = "360deg";
+    나변수.style.scale = "1.3";
+    // 트랜스폼 속성 한번에 작성하기:
+        // 나변수.style.transform = "translate(-50%, -50%) rotate(360deg) scale(1.3)";
+    // 만약 속성명을 잘못 작성하면 브라우저가 출력에서 제외한다
+    // 예전 브라우저는 에러가 발생했지만, 요즘에는 에러가 안난다
+
+    // 3-11. 전체 배경색 바꾸기
+    // 대상: body
+    // JS에서 body는 특별히 취급하여 document.body로 선택
+    var 나바디 = document.body;
+    나바디.style.backgroundColor = "lightblue";
+    // 나바디.style.backgroundColor = "linear-gradient(45deg, lightgreen, lightblue)";
+    나바디.style.transition = "3s 5s";
+
+} //////////// 김비서나와라 함수 //////////

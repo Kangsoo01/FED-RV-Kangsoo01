e1d415477dcc31bfd11ff06f45454f97f9472faa
Kangsoo01 <angelakang54@gmail.com>
2024-12-30 17:59:17 +0900

    24-12-30 업데이트

--- 700.쉐도잉_프로젝트/01.DKB_PJ/03.구현수집/js/main.js
+++ 700.쉐도잉_프로젝트/01.DKB_PJ/03.구현수집/js/main.js
@@ -9,7 +9,62 @@ import * as dkbData from "../data/dkb_data.js";
 // as로 별칭을 지어주면 객체화되어 담겨진다!
 // import { previewData } from "../data/dkb_data.js";
 
-console.log(dkbData);
+// console.log(dkbData);
+
+// 도깨비 GNB 데이터 불러오기
+import gnbData from "../data/gnb_data.js";
+console.log(
+  gnbData,
+  Object.keys(gnbData),
+  gnbData["About tvN"]
+);
+
+// 0. GNB 데이터 바인딩하기
+$(".gnb").html(`
+    <ul class="fx-box">
+      ${Object.keys(gnbData)
+        .map(
+          (v) => `
+        <li>
+          <a href="#">
+            ${
+              v +
+              (gnbData[v] == "없음"
+                ? ""
+                : '<i class="fa-solid fa-chevron-down"></i>')
+            }
+            
+          </a>
+          ${
+            gnbData[v] == "없음"
+              ? ""
+              : `
+                <!-- 서브메뉴 -->
+                <aside class="smenu">
+                  <div class="inbox">
+                    <h2>${v}</h2>
+                    <ol>
+                    ${gnbData[v]
+                      .map(
+                        (v2) => `
+                        <li>
+                          <a href="#">${v2}</a>
+                        </li>
+                      `
+                      )
+                      .join("")}
+                    </ol>
+                  </div>
+                </aside>
+                
+                `
+          }
+        </li>
+        `
+        )
+        .join("")}
+    </ul>
+  `);
 
 // 1. 슬라이드함수 호출하여 실행하기
 slideFn();
@@ -87,18 +142,47 @@ $(".poster-box").html(
       .map(
         (v) => `
     <li data-idx="${v.idx}">
-      <figure>
-        <img
-          src="./images/poster_img/${v.imgName}.jpg"
-          alt="${v.title}"
-        />
-        <figcaption>${v.title}</figcaption>
-      </figure>
+    <figure>
+    <img
+    src="./images/poster_img/${v.imgName}.jpg"
+    alt="${v.title}"
+    />
+    <figcaption>${v.title}</figcaption>
+    </figure>
     </li>
-  `
+    `
       )
       .join("") +
     "</ul>"
+);
+
+/////////////////////////////////////////////////
+/// 최신동영상영역 : 데이터 연결하여 태그 만들기 ///
+/////////////////////////////////////////////////
+
+// 대상: .clip-box
+$(".clip-box").html(
+  `<ul class="slide swiper-wrapper" data-db="clipData">
+    ${dkbData.clipData
+      .map(
+        (v) => `
+        <li class="swiper-slide" data-idx="${v.idx}">
+          <div class="clip-mv-box">
+            <img
+              src="./images/clip_img/${v.idx}.jpg"
+              alt="${v.subtit}"
+            />
+          </div>
+          <h4>
+            ${v.subtit}
+          </h4>
+          <h3>${v.title}</h3>
+        </li>        
+      `
+      )
+      .join("")}
+      </ul>
+    `
 );
 
 //////////////////////////////

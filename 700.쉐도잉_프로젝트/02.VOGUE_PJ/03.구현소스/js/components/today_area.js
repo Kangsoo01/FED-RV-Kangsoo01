b03915a6d242656fe3a1d709038e8faa48d354c2
Kangsoo01 <angelakang54@gmail.com>
2025-02-14 17:58:02 +0900

    25-02-14 업데이트

--- 700.쉐도잉_프로젝트/02.VOGUE_PJ/03.구현소스/js/components/today_area.js
+++ 700.쉐도잉_프로젝트/02.VOGUE_PJ/03.구현소스/js/components/today_area.js
@@ -7,28 +7,18 @@ import itemData from '../../data/item_data.json' with{type:'json'};
 
 // 2. 투데이 영역 구현하기 ///////
 
-// 2-1. 투데이 영역용 데이터수집하기
-// filter() 메서드는 조건이 맞을때 true리턴!
-// -> 결과: 필터링된 배열값
-const todayData = itemData
-  .filter((v) => {
-    if (v.mainCat == "today") return true;
-  })
-  // 2-2. 투데이 영역 데이터 정렬하기 : idx 오름차순
-  // sort((a,b)=>a.idx==b.idx?0:a.idx<b.idx?-1:1)
-  .sort((a, b) => (a.idx == b.idx ? 0 : a.idx < b.idx ? -1 : 1));
 
-// console.log('today데이터:',todayData);
-
-export const TodayAreaComp = Vue.component("today-area-comp", {
-  // 1. 템플릿
-  template: `
+export const TodayAreaComp = Vue.component(
+  "today-area-comp",
+  {
+    // 1. 템플릿
+    template: `
      <div id="today-area">
         <section class="inbox today-area">
           <h2 class="sub-tit">Today’s Stories</h2>
           <div class="post-list-today">
             <ul>
-                <li v-for="v in todayInfo">
+                <li v-for="v in this getData today">
                     <figure>
                         <img 
                         :src="v.imgSrc" 
@@ -45,17 +35,37 @@ export const TodayAreaComp = Vue.component("today-area-comp", {
         </section>
       </div>
     `,
-  // 2. 리턴함수 데이터
-  data() {
-    return {
-      // 투데이 영역 정보 데이터
-      todayInfo: todayData,
-    };
-  },
-  // 3. 메서드
-  methods: {},
-  // 4. 데이터셋업파트
-  created() {},
-  // 5. DOM 셋업파트
-  mounted() {},
-});
+    // 2. 리턴함수 데이터
+    data() {
+      return {
+        // 투데이 영역 정보 데이터
+        todayInfo: todayData,
+      };
+    },
+    // 3. 메서드
+    methods: {
+      // 선택데이터 가져오기 메서드
+      getData(dName) {
+        // dName - 데이터 이름
+        // 2-1. 투데이 영역용 데이터수집하기
+        // filter() 메서드는 조건이 맞을때 true리턴!
+        // -> 결과: 필터링된 배열값
+        const todayData = itemData
+          .filter((v) => {
+            if (v.mainCat == "today") return true;
+          })
+          // 2-2. 투데이 영역 데이터 정렬하기 : idx 오름차순
+          // sort((a,b)=>a.idx==b.idx?0:a.idx<b.idx?-1:1)
+          .sort((a, b) =>
+            a.idx == b.idx ? 0 : a.idx < b.idx ? -1 : 1
+          );
+
+        // console.log('today데이터:',todayData);
+      },
+    },
+    // 4. 데이터셋업파트
+    created() {},
+    // 5. DOM 셋업파트
+    mounted() {},
+  }
+);
